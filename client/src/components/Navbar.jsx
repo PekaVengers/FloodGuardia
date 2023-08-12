9feec1adb5e3d2e8dd4bd24ac3d5cf1be3c25d2d
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-13 01:08:44 +0530

    fix: navbar links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,30 @@
+import { NavLink } from "react-router-dom"
+
 const Header = () => {
 
   return (
     <header className='flex justify-between px-5 py-3 bg-[#00613D]'>
-      <a href="/" className='logo text-3xl font-bold py-3 text-white'>LOGO</a>
+      <NavLink to="/" className='logo text-3xl font-bold py-3 text-white'>LOGO</NavLink>
       <nav className="flex justify-between">
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white rounded" aria-current="page">Home</a>
+              <NavLink to="/" className="block py-2 pl-3 pr-4 text-white rounded" aria-current="page">Home</NavLink>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white rounded">Services</a>
+              <NavLink to="/map" className="block py-2 pl-3 pr-4 text-white rounded">Map</NavLink>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white rounded">Blogs</a>
+              <NavLink to="#" className="block py-2 pl-3 pr-4 text-white rounded">Blogs</NavLink>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white rounded">About</a>
+              <NavLink to="#" className="block py-2 pl-3 pr-4 text-white rounded">About</NavLink>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white rounded">Team</a>
+              <NavLink to="#" className="block py-2 pl-3 pr-4 text-white rounded">Team</NavLink>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white rounded">Contact</a>
+              <NavLink to="#" className="block py-2 pl-3 pr-4 text-white rounded">Contact</NavLink>
             </li>
           </ul>
         </div>
