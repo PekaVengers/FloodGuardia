013c43bd256f270a83a88401e94d6405f3dcf6cd
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-13 12:38:12 +0530

    style: navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Header = () => {
 
   return (
     <header className='flex justify-between items-center px-5 bg-[#00613D]'>
-      <NavLink to="/" className='logo text-3xl font-bold py-3 text-white'><img className="w-[15rem]" src={logo} /></NavLink>
+      <NavLink to="/" className='logo font-bold py-3'><img className="w-[10rem]" src={logo} /></NavLink>
       <nav className="flex justify-between">
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 text-[2rem]">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 text-[1.3rem]">
             <li>
               <NavLink to="/" className="block py-2 pl-3 pr-4 text-white rounded" aria-current="page">Home</NavLink>
             </li>
