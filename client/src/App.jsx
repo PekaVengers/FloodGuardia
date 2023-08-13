f42f039afc710c7e146030b6cdb02c55b0eeff5f
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-13 09:27:28 +0530

    feat: add donate page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Map from "./pages/Map";
+import Donate from "./components/Donate";
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route element={<Home />} index />
     <Route element={<Map />} path="maps" />
+    <Route element={<Donate />} path="donate" />
   </Route>
 ));
 
