3e047523580af6274da7892733bed7a3f10dba96
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-13 12:28:45 +0530

    keep the footer at the bottom

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -4,10 +4,12 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </>
+    <div className="flex flex-col min-h-screen">
+  <Navbar />
+  <div className="flex-1">
+    <Outlet />
+  </div>
+  <Footer />
+</div>
   );
 }
