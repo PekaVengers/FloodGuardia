70a3356db2175984cd2759d377d2fa447949eb20
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-13 11:17:48 +0530

    feat: add logo for website

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.PNG";
 
 const Header = () => {
 
   return (
-    <header className='flex justify-between px-5 py-3 bg-[#00613D]'>
-      <NavLink to="/" className='logo text-3xl font-bold py-3 text-white'>LOGO</NavLink>
+    <header className='flex justify-between items-center px-5 bg-[#00613D]'>
+      <NavLink to="/" className='logo text-3xl font-bold py-3 text-white'><img className="w-[15rem]" src={logo} /></NavLink>
       <nav className="flex justify-between">
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 text-[2rem]">
             <li>
               <NavLink to="/" className="block py-2 pl-3 pr-4 text-white rounded" aria-current="page">Home</NavLink>
             </li>
