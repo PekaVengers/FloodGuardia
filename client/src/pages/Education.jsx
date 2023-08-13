43c03401593a48f445088327e22f9646a050e86f
Vidip Ghosh <vidip2001@gmail.com>
2023-08-13 11:51:53 +0530

    3 more blogs added

--- client/src/pages/Education.jsx
+++ client/src/pages/Education.jsx
@@ -1,6 +1,9 @@
 import img5 from '../assets/ProtectPlanet.png'
 import img6 from '../assets/Boat.png'
 import img7 from '../assets/Windmill.png'
+import img8 from '../assets/WorldTrash.png'
+import img9 from '../assets/mangroove.png'
+import img10 from '../assets/renewable.png'
 
 const ytLinks = [
   {
@@ -21,23 +24,38 @@ export default function Education() {
   return (
     <div className='p-8'>
       <div className='flex justify-center'>
-        <button className='text-center text-lg bg-[#FFA765] p-3 font-bold mt-5 rounded-lg'>Some Blogs</button>
+        <button className='text-center text-lg bg-[#FFA765] p-3 font-bold mt-5 rounded-lg'>Learn About Flood Resilience</button>
       </div>
       <div className='grid md:grid-cols-3 p-8 m-auto'>
         <div className='p-8'>
-          <img className='rounded-md' src={img5} alt="" />
+          <img className='rounded-md h-72 w-[28rem]' src={img5} alt="" />
           <button className='text-center text-lg bg-[#FFD44A] p-3 font-bold mt-1 rounded-lg'>Environment</button><br />
           <a className='text-2xl underline' href="https://give.cmsmasters.net/ngo/2023/07/05/10-things-we-should-know-about-international-mother-earth-day/">10 Things We should Know about...</a>
         </div>
         <div className='p-8'>
-          <img className='rounded-md' src={img6} alt="" />
+          <img className='rounded-md h-72 w-[28rem]' src={img6} alt="" />
           <button className='text-center text-lg bg-[#FFD44A] p-3 font-bold mt-1 rounded-lg'>Environment</button><br />
           <a className='text-2xl underline' href="https://give.cmsmasters.net/ngo/2023/07/04/africa-climate-week-how-sustainable-farming-transforms-life-in-drc/">Africa Climate Week: How Sustainable</a>
         </div>
         <div className='p-8'>
-          <img className='rounded-md' src={img7} alt="" />
-          <button className='text-center text-lg bg-[#FFD44A] p-3 font-bold mt-1 rounded-lg'>Environment</button><br />
+          <img className='rounded-md h-72 w-[28rem]' src={img7} alt="" />
+          <button className='text-center text-lg bg-[#FFD44A] p-3 font-bold mt-1 rounded-lg'>Climate</button><br />
           <a className='text-2xl underline' href="https://give.cmsmasters.net/ngo/2023/07/02/why-climate-crisis-is-also-a-health-crisis/">Why Climate Crisis is also a Health Crisis</a>
+        </div>
+        <div className='p-8'>
+          <img className='rounded-md h-72 w-[28rem]' src={img8} alt="" />
+          <button className='text-center text-lg bg-[#FFD44A] p-3 font-bold mt-1 rounded-lg'>Environment</button><br />
+          <a className='text-2xl underline' href="https://give.cmsmasters.net/ngo/2023/07/05/dealing-with-the-worlds-waste-crisis/">Dealing with the World's Waste Crisis</a>
+        </div>
+        <div className='p-8'>
+          <img className='rounded-md h-72 w-[28rem]' src={img9} alt="" />
+          <button className='text-center text-lg bg-[#FFD44A] p-3 font-bold mt-1 rounded-lg'>Climate</button><br />
+          <a className='text-2xl underline' href="#">Mangrove Restoration for Coastal Protection</a>
+        </div>
+        <div className='p-8'>
+          <img className='rounded-md h-72 w-[28rem]' src={img10} alt="" />
+          <button className='text-center text-lg bg-[#FFD44A] p-3 font-bold mt-1 rounded-lg'>Climate</button><br />
+          <a className='text-2xl underline' href="https://give.cmsmasters.net/ngo/2023/07/02/asia-can-go-100-renewable/">Asia Can Go 100% Renewable</a>
         </div>
       </div>
       <div className='flex justify-center'>
