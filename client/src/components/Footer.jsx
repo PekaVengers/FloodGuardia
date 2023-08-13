61c02b42b0920d07d76f41f71ebb71bda9e45826
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-13 14:15:51 +0530

    change the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <div className="bg-[#00613D] h-[10vh] flex justify-around items-center">
       <div className="paraFooter">
-        <p className="font-bold">&copy; 2023 CareerCompass. All rights reserved.</p>
+        <p className="font-bold">&copy; 2023 FloodGuardia. All rights reserved.</p>
       </div>
       <div className="ghFooter">
         <a href="https://github.com/PekaVengers/CareerCompass"><img className="w-11" src={Github} alt="Follow on GitHub" /></a>
