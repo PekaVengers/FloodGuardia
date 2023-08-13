77a315c31341ccd5d4f6152bc757aa1c9977c51f
Kailash Choudhary <kailashjat604@gmail.com>
2023-08-13 11:52:57 +0530

    remove unnecessary code

--- client/src/components/Donate.jsx
+++ client/src/components/Donate.jsx
@@ -1,15 +1,7 @@
 export default function Donate() {
-    function App() {
-        const [selectedOption, setSelectedOption] = useState('');
-      
-        const handleOptionChange = (event) => {
-          setSelectedOption(event.target.value);
-        };
-    }
-    
-  return <div style={{fontWeight:'bold' , fontSize:'30px' , margin:'15px'}}>DONATE NOW
-    <br/>
-     <div className="flex justify-center">
+  return <div style={{ fontWeight: 'bold', fontSize: '30px', margin: '15px' }}>DONATE NOW
+    <br />
+    <div className="flex justify-center">
       <p className="bg-[#E4E4E4] p-5 w-fit flex text-xl">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +21,7 @@ export default function Donate() {
         <b>Notice:</b> Test mode is enabled. While in test mode no live donations are processed.
       </p>
     </div>
-    <br/>
+    <br />
     <input
       name="val1"
       className="  border rounded w-full py-2 px-3 text-gray-700 leading-tight  focus:outline-none focus:shadow-outline"
@@ -38,21 +30,21 @@ export default function Donate() {
       placeholder="Rs. 1000.00"
       value=""
     />
-    <br/><div className="" style={{display:'flex' , justifyContent:'center'}}>
-    <div className=""><button type="submit" style={{backgroundColor:'green', color:'white' ,  borderRadius: '10px'  , fontSize:'20px' , fontWeight:'' , margin:'20px' , padding:'5px' , width:'300px' ,  }}>Rs. 100.00</button>
-    </div>
-    <div className=""><button type="submit" style={{backgroundColor:'green', color:'white' ,  borderRadius: '10px'  , fontSize:'20px' , fontWeight:'' , margin:'20px' , padding:'5px' , width:'300px' , }}>Rs. 500.00</button>
-    </div>
-    <div className=""><button type="submit" style={{backgroundColor:'green', color:'white' ,  borderRadius: '10px'  , fontSize:'20px' , fontWeight:'' , margin:'20px' , padding:'5px' , width:'300px' ,  }}>Rs. 1000.00</button>
-    </div>
-    </div>
-    <div className="">
-        <h4 style={{margin:'20px'}}>Select Payment Method</h4>
-        
-          
+    <br /><div className="" style={{ display: 'flex', justifyContent: 'center' }}>
+      <div className=""><button type="submit" style={{ backgroundColor: 'green', color: 'white', borderRadius: '10px', fontSize: '20px', fontWeight: '', margin: '20px', padding: '5px', width: '300px', }}>Rs. 100.00</button>
+      </div>
+      <div className=""><button type="submit" style={{ backgroundColor: 'green', color: 'white', borderRadius: '10px', fontSize: '20px', fontWeight: '', margin: '20px', padding: '5px', width: '300px', }}>Rs. 500.00</button>
+      </div>
+      <div className=""><button type="submit" style={{ backgroundColor: 'green', color: 'white', borderRadius: '10px', fontSize: '20px', fontWeight: '', margin: '20px', padding: '5px', width: '300px', }}>Rs. 1000.00</button>
+      </div>
     </div>
     <div className="">
-        <h4 style={{margin:'20px'}}>Personal info</h4>
+      <h4 style={{ margin: '20px' }}>Select Payment Method</h4>
+
+
+    </div>
+    <div className="">
+      <h4 style={{ margin: '20px' }}>Personal info</h4>
     </div>
     <input
       name="val1"
@@ -62,21 +54,21 @@ export default function Donate() {
       placeholder="Name"
       value=""
     />
-     <input
+    <input
       name="val1"
       className="  border rounded w-full  mt-5 py-2 px-3 text-gray-700 leading-tight  focus:outline-none focus:shadow-outline"
       id="username"
       type="text"
-      
+
       placeholder="Email"
       value=""
     />
-    
-            
 
-       
-         
+
+
+
+
   </div>
- 
-    
+
+
 }
